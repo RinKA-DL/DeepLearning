c5dbd8eeebfd7e30c8a361111c16605a45c24b41
gin1114 <someonetolove1114@gmail.com>
2018-07-16 18:38:46 +0900

    textbook file

---
+++ work/gin1114/ReLU1.py
@@ -0,0 +1,3 @@
+import numpy as np
+def ReLU(x):
+    return np.maximum(0,x)
---
+++ work/gin1114/numpy1.py
@@ -0,0 +1,9 @@
+import numpy as np
+x=np.array([1,2,3])
+print(x)
+print(type(x))
+
+A=np.array([[1, 2], [3, 4]])
+print(A)
+print(A.shape)
+print(A.dtype)
